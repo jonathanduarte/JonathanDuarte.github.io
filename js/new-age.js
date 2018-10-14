5d578c481218a809e53315544c7a4b0958968cc0
Jonathan Duarte <jonathan.duarte@synergy-gb.com>
2018-10-14 08:35:58 -0400

    flecha 4

--- js/new-age.js
+++ js/new-age.js
@@ -1,6 +1,12 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  //PHP sender
+
+    $("#mc-embedded-subscribe-form").bind('ajax:complete', function() {
+        $("#exampleModal").modal();
+      });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
